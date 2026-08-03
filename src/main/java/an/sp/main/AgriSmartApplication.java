75fe99c88c06e3be9156015eddf076abd3278e0f
Anand Patel <166007508+anandptl@users.noreply.github.com>
2026-08-03 19:54:34 +0530

    last 7

--- src/main/java/an/sp/main/AgriSmartApplication.java
+++ src/main/java/an/sp/main/AgriSmartApplication.java
@@ -2,13 +2,18 @@ package an.sp.main;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class AgriSmartApplication {
+public class AgriSmartApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AgriSmartApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AgriSmartApplication.class, args);
-        System.out.println("AgriSmartApplication started successfully!");
     }
-
 }
